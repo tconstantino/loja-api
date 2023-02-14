567bc9d7367082b69fb44b02b11174357a9f99cb
thiago constantino <thiagoconstantino@thiagos-MacBook-Pro.local>
2023-02-13 22:25:18 -0300

    Validacao de insercao de usuario

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CriarUsuarioDTO } from './dto/criarUsuario.dto';
 import { UsuarioRepository } from './usuario.repository';
 
 @Controller('/usuarios')
@@ -6,7 +7,7 @@ export class UsuarioController {
   constructor(private usuarioRepository: UsuarioRepository) {}
 
   @Post()
-  async criarUsuario(@Body() usuario) {
+  async criarUsuario(@Body() usuario: CriarUsuarioDTO) {
     this.usuarioRepository.salvar(usuario);
     return { mensagem: 'usuario criado!', usuario };
   }
